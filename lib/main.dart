b80abd2f7ee2d067a00197246613c6438ad9d5c6
erikmiqueias <erikmiqueiaspereira@outlook.com>
2025-05-09 00:06:05 -0400

    feat: screen orientation locked

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stopwatch/stopwatch.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MaterialApp(home: const Stopwatch(title: 'Stopwatch')));
 }
